5d50c0129bf84eae10d88e2907af1a00f9c64bd8
Karina Bekbayeva <35498147+karinabk@users.noreply.github.com>
2018-06-17 01:15:26 +0600

    Binary search practice

---
+++ binary_search.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binary_search(std::vector<int> v,int num){
+  int lo=0, hi=v.size()-1;
+  while(lo<=hi){
+    int mid = lo+(hi-lo)/2;
+    if(num==v[mid]){
+      return mid;
+    }
+    if(num>v[mid]){
+      lo=mid+1;
+    }
+    if(num<v[mid]){
+      hi=mid-1;
+    }
+  }
+  return -1;
+}
+
+int binary_search_recursion(std::vector<int> v,int hi, int lo, int num){
+  if(v[mid]==num){
+    return mid
+  }
+  if(lo>hi){
+    return -1;
+  }
+  int mid = lo+(hi-lo)/2;
+  if(num>v[mid]){
+    return binary_search_recursion(v,hi,mid+1,num);
+  }
+  if(num<v[mid]){
+    return binary_search_recursion(v,mid-1,lo,num);
+  }
+}
+
+
+int main(){
+
+  std::vector<int> v(15) ;
+  for(int i = 0;i<14;i++){
+    v[i]=i*3;
+  }
+  std::cout<<"Value: "<< i*3<<"Index: "<< i<<endl;
+  std::cout <<"binary search"<<binary_search(v,1) << '\n';
+
+
+}
